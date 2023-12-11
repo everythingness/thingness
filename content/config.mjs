cae4292a4e69cd44e48931177061890f6c10f26d
@everythingness <89503536+everythingness@users.noreply.github.com>
2023-12-11 11:33:47 +0100

    Update config.mjs

--- content/config.mjs
+++ content/config.mjs
@@ -1,1 +1,11 @@
-export default {};
+const config = {
+  title: "nothingness",
+  description: "it's nothing, really.w",
+  author: "",
+  logo: "/images/logo.svg",
+  domain: "https://no.thingness.link",
+  // links to the pages you want to link to in the navbar and in the footer
+  navLinks: [{ href: "/about", name: "about nothing" }],
+};
+
+export default config;
